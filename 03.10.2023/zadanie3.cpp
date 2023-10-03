3a60b6953237ac4dd89252c67dc8a050613a52f2
Zwirekk <121910474+Zwirekk@users.noreply.github.com>
2023-10-03 15:14:46 +0200

    Add files via upload
    zadanie 3

---
+++ 03.10.2023/zadanie3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, c, wyraz;
+	cout << "Wprowadz pierwszy bok trojkatu: ";
+	cin >> a;
+	cout << "Wprowadz drugi bok trojkatu: ";
+	cin >> b;
+	cout << "Wprowadz trzeci bok trojkatu: ";
+	cin >> c;
+	if (a == 0 || b == 0 || c == 0)
+		cout << "Blad, wprowadzono wartosc zero";
+	else
+		if (a + b > c && a + c > b && b + c > a)
+			cout << "Z podanych dlugosci da zrobic sie trojkat";
+		else
+			cout << "Z podanych dlugosci nie da zrobic sie trojkat";
+}
