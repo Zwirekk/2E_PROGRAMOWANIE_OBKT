5103b42c4728b12ec82d74594419d3e9ead29e55
Zwirekk <121910474+Zwirekk@users.noreply.github.com>
2023-10-10 14:45:35 +0200

    Add files via upload

---
+++ 10.10.2023/zadanie1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	string login = "admin";
+	string haslo = "haslo123";
+	cout << "Wprowadź login: ";
+	cin >> login;
+	if (login != "admin") {
+		cout << "Niepoprawny login, nie przyznano dostepu!";
+		goto exit;
+	}
+	cout << "Wprowadź hasło: ";
+	cin >> haslo;
+	if (haslo != "haslo123") {
+		cout << "Niepoprawne haslo, nie przyznano dostepu!";
+		goto exit;
+	}
+	cout << "Przyznano dostep";
+
+	exit:
+		return 0;
+}
---
+++ 10.10.2023/zadanie2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+int main() {
+	unordered_map<string, string> loginHaslo;
+	string login, haslo;
+
+	loginHaslo["login1"] = "haslo1";
+	loginHaslo["login2"] = "haslo2";
+	loginHaslo["login3"] = "haslo3";
+
+	int probylogin = 3;
+	int probyhaslo = 3;
+	while (probylogin > 0 && probyhaslo > 0) {
+		cout << "Podaj nazwe uzytkownika: ";
+		cin >> login;
+
+		if (loginHaslo.find(login) != loginHaslo.end()) {
+			while (probyhaslo > 0) {
+				cout << "Podaj haslo: ";
+				cin >> haslo;
+
+				if (loginHaslo[login] == haslo) {
+					cout << "Zalogowana jako " << login << endl;
+					goto exit;
+				}
+				else {
+					cout << "Nieprawidlowe haslo" << endl; probyhaslo--;
+				}
+			}
+		}
+		else {
+			cout << "Nieprawidlowa nazwa uzytkownika." << endl; probylogin--;
+		}	
+	}
+
+	if (probylogin == 0 || probyhaslo == 0) {
+		cout << "Przekroczono limit prob zalogowania!";
+	}
+exit:
+	return 0;
+
+}
