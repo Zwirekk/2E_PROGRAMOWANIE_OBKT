58ae2ac6fb74436c929f06765a6e37c3065275f6
Zwirekk <121910474+Zwirekk@users.noreply.github.com>
2023-10-26 12:45:08 +0200

    Create zadanie1.cpp

---
+++ 26.10.23/zadanie1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 0, tab[31], liczba;
+    
+    cout << "Podaj liczbę: ";
+    cin >> liczba;
+    
+    while(liczba != 0)
+    {
+        tab[i++] = liczba % 2;
+        liczba /= 2;
+    }
+    for (int j = i - 1; j >= 0; j--)
+    {
+        cout << tab[j];
+    }
+}
