5566a8c23db7addd28712e9b53d57515e4ee2ca9
Zwirekk <121910474+Zwirekk@users.noreply.github.com>
2024-03-07 12:00:24 +0100

    Create zadanie3.cpp

---
+++ 07.03.24/zadanie3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+void iloczyn(int liczba1, int liczba2)
+{
+    cout << "iloczyn dwoch liczb to: " << liczba1 * liczba2;
+}
+
+int main()
+{
+    iloczyn(12, 5);
+}
