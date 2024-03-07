77b77004763a6ae103004b2af8c884ac11596bc1
Zwirekk <121910474+Zwirekk@users.noreply.github.com>
2024-03-07 11:59:13 +0100

    Create zadanie2.cpp

---
+++ 07.03.24/zadanie2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+void roznica(int liczba1, int liczba2)
+{
+    cout << "roznica dwoch liczb to: " << liczba1 - liczba2;
+}
+
+int main()
+{
+    roznica(12, 5);
+}
