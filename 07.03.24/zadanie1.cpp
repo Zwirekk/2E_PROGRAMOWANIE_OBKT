eaafc50e8f6a77dadf1dc458c269ba79b83e09be
Zwirekk <121910474+Zwirekk@users.noreply.github.com>
2024-03-07 11:58:33 +0100

    Create zadanie1.cpp

---
+++ 07.03.24/zadanie1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+void suma(int liczba1, int liczba2)
+{
+    cout << "suma dwoch liczb to: " << liczba1 + liczba2;
+}
+
+int main()
+{
+    suma(12, 5);
+}
