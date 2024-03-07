c523f537a1a5bf6a2fecf4d27ae71be54e4861e1
Zwirekk <121910474+Zwirekk@users.noreply.github.com>
2024-03-07 12:01:09 +0100

    Create zadanie4.cpp

---
+++ 07.03.24/zadanie4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+void iloraz(int liczba1, int liczba2)
+{
+    cout << "iloraz dwoch liczb to: " << liczba1 / liczba2;
+}
+
+int main()
+{
+    iloraz(10, 5);
+}
