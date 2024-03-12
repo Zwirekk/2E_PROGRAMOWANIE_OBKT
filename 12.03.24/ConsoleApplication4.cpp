2eb0adcb12456b38a06ceaf4db8f50622ac296ef
Zwirekk <121910474+Zwirekk@users.noreply.github.com>
2024-03-12 15:07:08 +0100

    Add files via upload

---
+++ 12.03.24/ConsoleApplication4.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <ctime>
+using namespace std;
+
+int main() {
+
+	/* 10.3
+	const char* euler = "2.7182818284590452353602874713526624977";
+
+	cout << atof(euler);
+	*/
+
+	/* 10.4
+	int bok1, bok2, pole, obwod;
+
+	printf("Podaj dlugosc pierwszego boku: ");
+	scanf_s("%d", &bok1);
+	printf("Podaj dlugosc drugiego boku: ");
+	scanf_s("%d", &bok2);
+
+	pole = bok1 * bok2;
+	obwod = 2 * bok1 + 2 * bok2;
+	
+	printf("Pole wynosi: %6d\n", pole);
+	printf("Obwod wynosi: %6d\n", obwod);
+	*/
+
+	/* 10.5
+	srand(time(NULL));
+	cout << (rand() % 901 + 100) << endl;
+	*/
+
+
+	// 1
+
+}
