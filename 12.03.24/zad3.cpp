1165546dae48e0c489ecb8c92e68ac43b4921be3
Zwirekk <121910474+Zwirekk@users.noreply.github.com>
2024-03-19 14:51:20 +0100

    Update zad3.cpp

--- 12.03.24/zad3.cpp
+++ 12.03.24/zad3.cpp
@@ -3,36 +3,39 @@
 
 using namespace std;
 
-int main()
-{
+struct {
     char imie[20];
     char nazwisko[20];
     char plec[10];
+} dane;
+
+int main()
+{
 
     printf("Podaj imie pracownika: ");
-    scanf_s("%s", imie, 20);
+    scanf_s("%s", dane.imie, 20);
 
     printf("Podaj nazwisko pracownika: ");
-    scanf_s("%s", nazwisko, 20);
+    scanf_s("%s", dane.nazwisko, 20);
     do {
-            printf("(m/M - mezczyzna, k/K - kobieta\nPodaj plec pracownika: ");
-            scanf_s("%s", plec, 2);
-            if (plec[0] != 'm' && plec[0] != 'M' && plec[0] != 'k' && plec[0] != 'K')
-            {
-                printf("Podana zla plec!\n");
-            }
-} while (plec[0] != 'm' && plec[0] != 'M' && plec[0] != 'k' && plec[0] != 'K');
+        printf("(m/M - mezczyzna, k/K - kobieta\nPodaj plec pracownika: ");
+        scanf_s("%s", dane.plec, 2);
+        if (dane.plec[0] != 'm' && dane.plec[0] != 'M' && dane.plec[0] != 'k' && dane.plec[0] != 'K')
+        {
+            printf("Podana zla plec!\n");
+        }
+    } while (dane.plec[0] != 'm' && dane.plec[0] != 'M' && dane.plec[0] != 'k' && dane.plec[0] != 'K');
 
-    if (plec[0] == 'm' || plec[0] == 'M')
+    if (dane.plec[0] == 'm' || dane.plec[0] == 'M')
     {
-        printf("Imie: %s", imie);
-        printf(" Nazwisko: %s", nazwisko);
+        printf("\nImie: %s", dane.imie);
+        printf(" Nazwisko: %s", dane.nazwisko);
         printf(" Plec: Mezczyzna");
     }
     else
     {
-        printf("Imie: %s", imie);
-        printf(" Nazwisko: %s", nazwisko);
+        printf("\nImie: %s", dane.imie);
+        printf(" Nazwisko: %s", dane.nazwisko);
         printf(" Plec: Kobieta");
     }
 
