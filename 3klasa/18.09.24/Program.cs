0d4d9b0136fdf5a5f9c6c08f6585afb0ee3c12c5
Zwirekk <121910474+Zwirekk@users.noreply.github.com>
2024-09-18 13:12:09 +0200

    Add files via upload

---
+++ 3klasa/18.09.24/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace dupa
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //1
+            float pi = float.Pi;
+
+            //2
+            string dane = "Filip Wira";
+
+            //3
+            int x = 10;
+            float y = (float)x;
+
+            //4
+            double a = 10.78493;
+            int b = Convert.ToInt32(a);
+
+            //5
+            Console.WriteLine("Podaj napis: ");
+            string napis = Console.ReadLine();
+            Console.WriteLine("Twoj napis: " + napis);
+        }
+    }
+}
+
